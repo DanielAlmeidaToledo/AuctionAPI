9f1def1abf1c5a9f8adee216008f8cdeec294f4f
Daniel Toledo <dan992965@gmail.com>
2024-02-24 02:55:56 -0300

    feat> adiciona Controller Auction

---
+++ src/AuctionProject.API/Controllers/AuctionController.cs
@@ -0,0 +1,20 @@
+﻿using AuctionProject.API.UseCases.Auctions.GetCurrent;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCurrentAuction()
+        {
+            var useCase = new GetCurrentAuctionUseCase();
+
+            var result = useCase.Execute();
+
+            return Ok(result);
+        }
+    }
+}
