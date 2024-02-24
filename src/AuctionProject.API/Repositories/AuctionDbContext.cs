980cda566efebfbe2474943312f24409ee0f05eb
Daniel Toledo <dan992965@gmail.com>
2024-02-24 02:56:27 -0300

    feat: adiciona início da conexão com db

---
+++ src/AuctionProject.API/Repositories/AuctionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuctionProject.API.Entities;
+
+namespace AuctionProject.API.Repositories
+{
+    public class AuctionDbContext : DbContext
+    {
+        public DbSet<Auction> Auctions { get; set; }
+
+        /*TODO - CONFIGURAR DB*/
+    }
+}
---
+++ src/AuctionProject.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,15 @@
+﻿using AuctionProject.API.Entities;
+using AuctionProject.API.Repositories;
+
+namespace AuctionProject.API.UseCases.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCase
+    {
+        public Auction Execute()
+        {
+            var repository = new AuctionDbContext();
+
+            return repository.Auctions.First();
+        }
+    }
+}
